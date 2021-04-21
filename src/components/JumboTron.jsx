9f08985347e1b3c83e283b6e8a9d2d92536bc465
Sergio Gomez <sergiogomez100000@gmail.com>
2021-04-20 17:02:04 -0700

    rendering jumbotron

---
+++ src/components/JumboTron.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+function JumboTron() {
+  return <>
+    <div className="jumbotron jumbotron-fluid">
+      <div className="container">
+        <h1 className="display-4">Employee Directory</h1>
+        <p className="lead">A directory for employers to view all their employees and info!</p>
+      </div>
+    </div>
+  </>
+}
+
+export default JumboTron;
