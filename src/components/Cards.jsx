a37231dc9cfc5b8f5be1b0a40075e8629c83707c
Sergio Gomez <sergiogomez100000@gmail.com>
2021-04-20 17:39:09 -0700

    card layout done

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -3,11 +3,13 @@ import React from 'react';
 
 function Cards() {
     return <>
-        <div className ="card">
-            <img className="card-img-top" src="..." alt="Card image cap" />
+        <div className ="card" style={{width:"8rem"}}>
+            <img className="card-img-top" src="https://randomuser.me/api/portraits/thumb/men/75.jpg" alt="employee-photo" />
             <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <h5 className="card-title">Employee Name</h5>
+                <p className="card-text">Email</p>
+                <p className="card-text">Phone</p>
+                <p className="card-text">Address</p>
             </div>
         </div>
     </>
