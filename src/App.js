239cc2d904c8a1c4530eb58464aaeb9405d821ea
Sergio Gomez <sergiogomez100000@gmail.com>
2021-04-22 16:21:35 -0700

    setting up now

--- src/App.js
+++ src/App.js
@@ -1,32 +1,57 @@
-import React from 'react';
-import JumboTron from './components/JumboTron';
-import Table from './components/Table';
-import API from "./utils/API"
-
+import React from "react";
+import JumboTron from "./components/JumboTron";
+import Table from "./components/Table";
+import API from "./utils/API";
 
 class App extends React.Component {
   state = {
     employees: [],
+    searched: []
   };
 
-  componentDidMount() {
-    this.getEmployees();
+   async componentDidMount() {
+    const { data } = await API.getUsers();
+    this.setState({ employees: data.results, searched: data.results });
   }
 
-  getEmployees = async () => {
-    const { data } = await API.getUsers();
-    this.setState({ employees: data.results });
+  handleSort = () =>{
+    const sortedEmp = this.state.searched.sort((a,b) => a.name.first > b.name.first ? 1: -1)
+    this.setState({searched:sortedEmp})
+  }
+
+  InputChange = (event) => {
+    const value = event.target.value.toLowerCase();
+    const searchedArr = this.state.employees.filter(emp =>emp.name.first.toLowerCase().startsWith(value))
+    this.setState({
+      searched: searchedArr,
+    });
   };
 
   render() {
     console.log(this.state);
-    return (<> <JumboTron />
-      {this.state.employees.length > 0 && 
-      this.state.employees.map((employees) => (<Table image={employees.picture.thumbnail} name={employees.name} phone={employees.cell} email={employees.email} dob={employees.dob.date.slice(0,10)}/>))};
-
-    </>
-    )
-  
-    }
+    return (
+      <>
+        {" "}
+        <JumboTron />
+        <input
+          type="search"
+          className="form-control"
+          placeholder="Search for an employee name"
+          onChange={this.InputChange}
+        />
+        <button onClick={this.handleSort}>Sort A-Z!!! </button>
+        {this.state.searched.length > 0 &&
+          this.state.searched.map((employees) => (
+            <Table
+              image={employees.picture.thumbnail}
+              name={employees.name}
+              phone={employees.cell}
+              email={employees.email}
+              dob={employees.dob.date.slice(0, 10)}
+            />
+          ))}
+      </>
+    );
   }
-  export default App;
+}
+export default App;
--- src/components/Table.js
+++ src/components/Table.js
@@ -1,46 +1,31 @@
-
-import React from 'react';
+import React from "react";
 
 const styles = {
-    background:"lightblue",
-    border: "1px solid black"
-}
+  background: "lightblue",
+  border: "1px solid black",
+};
 function Table(props) {
-        return (
-          <table class="table"style={styles}>
-            <thead>
-              <tr>
-                <th scope="col">Image</th>
-                <th scope="col">Name</th>
-                <th scope="col">Phone</th>
-                <th scope="col">E-Mail</th>
-                <th scope="col">DOB</th>
-              </tr>
-            </thead>
-            <tbody>
-                  <tr>
-                    <td><img src= {props.image} /></td>
-                    <td>{props.name.first} {props.name.last}</td>
-                    <td>{props.phone}</td>
-                    <td>{props.email}</td>
-                    <td>{props.dob}</td>
-                  </tr>
-            </tbody>
-          </table>
-        );
-      
-
-    // return <>
-    //     <div className ="card" style={styles}>
-    //         <img className="card-img-top"style={{width:"50%"}} src={props.image} alt="employee-photo" />
-    //         <div className="card-body">
-    //             <h5 className="card-title">{props.name.first} {props.name.last}{" "}</h5>
-    //             <p className="card-text">{props.email}</p>
-    //             <p className="card-text">{props.phone}</p>
-    //             <p className="card-text">{props.dob}</p>
-    //         </div>
-    //     </div>
-    // </>
+  return (
+    <table class="table" style={styles}>
+      <thead>
+        <tr>
+          <th scope="col">Image</th>
+          <th scope="col">Name</th>
+          <th scope="col">Phone</th>
+          <th scope="col">E-Mail</th>
+          <th scope="col">DOB</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td><img src={props.image}/></td>
+          <td>{props.name.first} {props.name.last}</td>
+          <td>{props.phone}</td>
+          <td>{props.email}</td>
+          <td>{props.dob}</td>
+        </tr>
+      </tbody>
+    </table>
+  );
 }
-
 export default Table;
